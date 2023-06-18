4703a55c6d9e0715f6704427418a65f9a9eee26c
TheTheMarcos <thethemarcos@gmail.com>
2023-06-18 15:21:25 +0200

    MergeSquash Colaborador

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("1º Main");
         System.out.printf("2º Main");
+        System.out.printf("1º Colaborador");
+        System.out.printf("2º Colaborador");
+        System.out.printf("3º Colaborador");
     }
 }
